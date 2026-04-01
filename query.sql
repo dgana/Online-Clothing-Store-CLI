952e163ab23f1bf7572604a1ee49cafb24dcaea3
Felias Danu <feliasdanu@gmail.com>
2026-04-01 20:30:36 +0700

    nambah query.sql

---
+++ query.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE FYGAR;
+USE FYGAR;
+
+-- 1. USERS (WAJIB ADA)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- 2. PROFILE (1-1 dengan users)
+CREATE TABLE profiles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNIQUE,
+    name VARCHAR(100),
+    address TEXT,
+    phone VARCHAR(20),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- 3. PRODUCTS
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    price DECIMAL(10,2),
+    stock INT
+);
+
+-- 4. ORDERS (1 user bisa banyak order)
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- 5. ORDER ITEMS (Many-to-Many)
+CREATE TABLE order_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+
